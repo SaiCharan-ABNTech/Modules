f896d88322f79b02f5dea6b93a208a3b511504e3
SaiCharan-ABNTech <154917195+SaiCharan-ABNTech@users.noreply.github.com>
2024-08-27 19:59:50 +0530

    Updated main.tf

--- AWS_Route_53/modules/main.tf
+++ AWS_Route_53/modules/main.tf
@@ -6,6 +6,15 @@ resource "aws_route53_zone" "this" {
   name = var.domain_name
 }
 
+resource "aws_cloudwatch_log_group" "dns_query_logs" {
+  name = "/aws/route53/query-logs"
+}
+
+resource "aws_route53_query_log" "this" {
+  zone_id                  = aws_route53_zone.this.id
+  cloudwatch_log_group_arn = aws_cloudwatch_log_group.dns_query_logs.arn
+}
+
 resource "aws_route53_record" "a_record" {
   zone_id = aws_route53_zone.this.zone_id
   name    = var.record_name
@@ -21,4 +30,5 @@ resource "aws_route53_record" "cname_record" {
   ttl     = var.ttl
   records = var.cname_record_values
 }
+
 
